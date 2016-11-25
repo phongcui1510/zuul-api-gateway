ab0f325685bd9595f6e56b2df8b6e3a89789a57f
Phong Nguyen <mrphongnt90@gmail.com>
2016-11-26 00:28:12 +0700

    aaaaa

--- src/main/java/com/mechanixlab/ApiGatewayApplication.java
+++ src/main/java/com/mechanixlab/ApiGatewayApplication.java
@@ -2,6 +2,8 @@ package com.mechanixlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -9,11 +11,16 @@ import com.mechanixlab.filters.pre.PreFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class ApiGatewayApplication {
+public class ApiGatewayApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApiGatewayApplication.class);
+    }
 
     @Bean
     public PreFilter preFilter() {
